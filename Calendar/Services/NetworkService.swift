66927803014da9ccc0a669a2dd7268edf3022850
danyaDerdan <110595066+danyaDerdan@users.noreply.github.com>
2025-03-03 21:56:28 +0300

    NetworkManager done

--- Calendar/Services/NetworkService.swift
+++ Calendar/Services/NetworkService.swift
@@ -1,7 +1,26 @@
-//
-//  NetworkService.swift
-//  Calendar
-//
-//  Created by Данил Толстиков on 27.11.2024.
-//
+import Foundation
+
+protocol NetworkServiceProtocol {
+    func fetchImage(stringUrl: String, completion: @escaping (Result<Data, Error>) -> Void)
+}
+
+final class NetworkService: NetworkServiceProtocol {
+    func fetchImage(stringUrl: String, completion: @escaping (Result<Data, Error>) -> Void) {
+        guard let url = URL(string: stringUrl) else {
+            completion(.failure(Errors.invalidUrl))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error { completion(.failure(error)) }
+            guard let data else { completion(.failure(Errors.invalidData)); return }
+            completion(.success(data))
+        }.resume()
+    }
+}
+
+private enum Errors: Error {
+    case invalidUrl
+    case invalidData
+}
 
